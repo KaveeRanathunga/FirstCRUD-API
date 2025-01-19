e2026ae6c1167127647623c2ad0822371af44cb0
KaveeRanathunga <156744773+KaveeRanathunga@users.noreply.github.com>
2025-01-19 19:18:39 +0530

    Done

--- index.js
+++ index.js
@@ -14,100 +14,14 @@ app.use('/api/products',productRoute);
  
 
 
-
 app.get('/', (req,res) => {
     res.send("Hello from Node API server Updated");
 })
 
 
 
-//Get all products
-app.get('/api/products', async (req,res) => {
 
-    try{
-
-        const products = await Product.find({});
-        res.status(200).json(products);
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-});
-
-
-
-//Get one by one
-app.get('/api/products/:id', async (req,res) =>{
-    try{
-
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-});
-
-
-
-//create Products
-app.post('/api/products', async (req,res) => {
-    try{
-
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-
-    }catch(error){
-
-        res.status(500).json({message: error.message});
-    }
-    
-})
-
-//update a product
-
-app.put('/api/products/:id', async(req,res) =>{
-    try{
-
-        const {id} = req.params;
-
-        const product = await Product.findByIdAndUpdate(id,req.body);
-
-        if(!product){
-            return res.status(404).json({message: "Product nor found"});
-
-        }
-
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-});
-
-
-app.delete('/api/products/:id', async (req,res) => {
-    try{
-
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-
-        if(!product){
-
-           return res.status(404).json({message:"product not found"});
-
-        }
-
-        res.status(200).json({message:"Product deleted successfully"});
-
-
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-});
-
-
+ 
 mongoose.connect("mongodb+srv://priyanjithranathunga:nXx9tCMkM5qQbxHY@crudapi.c3dgq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUDAPI").then(() => {
 
    
